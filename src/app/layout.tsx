0474c161efbabfc48dff1d214a3400852842fcb2
Kritika Srivastava <kritikasrivastava.pis@gmail.com>
2026-03-31 20:13:18 +0000

    also where should i place pop under? (<script src="https://pl29029576.pr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -20,13 +19,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        
-        {/* Adsterra Social Bar Placeholder - Paste your script here */}
-        {/* <Script id="adsterra-social-bar" strategy="afterInteractive">
-          {`
-            // Paste your Adsterra Social Bar code here
-          `}
-        </Script> */}
+
+        {/* Adsterra Pop-Under Script */}
+        <Script 
+          src="https://pl29029576.profitablecpmratenetwork.com/f1/f0/d4/f1f0d4771e816260a95c95d69d12f208.js" 
+          strategy="afterInteractive" 
+        />
+
+        {/* 
+          Placeholder for Adsterra Social Bar: 
+          If you have another script for the Social Bar, 
+          you can add it right here using another <Script /> tag.
+        */}
       </head>
       <body className="font-body antialiased bg-background text-foreground selection:bg-accent/30">
         {children}
